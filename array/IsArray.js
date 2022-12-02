f6e97b74ce44b8026ced1d76a17d7a02ee8e5372
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-12-02 14:51:10 +0530

    made new file called IsArray

---
+++ array/IsArray.js
@@ -0,0 +1,6 @@
+// The Array.isArray() static method determines whether the passed value is an Array.
+
+const checkarr = ["Grapes","Butter Milk","Banana"];
+const arrval = Array.isArray(checkarr);
+// Expected output "true";
+console.log(arrval);
