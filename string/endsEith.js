9f1b860e600dff441ffc96bfb3649c158da84fb3
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-12-09 12:05:46 +0530

    learned new methods in string

---
+++ string/endsEith.js
@@ -0,0 +1,6 @@
+// The endsWith() method determines whether a string ends with the characters of a specified string,
+//  returning true or false as appropriate.
+
+const fruits = ["Apple","Banana","Grapes","Butter Milk"];
+console.log(fruits.endsWith("Butter Milk"))
+// Expexted output true;
