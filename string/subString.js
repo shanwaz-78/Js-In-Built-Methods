4626d0d0e0db828bbab45ade4a89519a9ec76f68
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-12-11 01:07:41 +0530

    learned new string methods

---
+++ string/subString.js
@@ -0,0 +1,7 @@
+// The substring() method returns the part of the string from the start index up to and excluding the end index, 
+// or to the end of the string if no end index is supplied.
+
+const str = "This Is Just Text";
+console.log(str.substring(2,9));
+ // Expected output "is Is J";
+ 
---
+++ string/trim.js
@@ -0,0 +1,6 @@
+// The trim() method removes whitespace from both ends of a string and returns a new string, 
+// without modifying the original string.
+
+const str = "   This Is Just String   ";
+console.log(str.trim());
+// Expected output "This Is Just String;
