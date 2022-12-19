6432dd0daf19966b2d95d5576c0144328dd6943b
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-12-19 19:29:04 +0530

    learned new methods of obj"

---
+++ object/object.fromEntries.js
@@ -0,0 +1,10 @@
+// The Object.fromEntries() method transforms a list of key-value pairs into an object.
+
+const entries = [
+    ['Fruit','Apple'],
+    ['Dairy','Milk'],
+];
+
+// He transforms the values of lists into object.
+const val = Object.fromEntries(entries);
+console.log(val);
