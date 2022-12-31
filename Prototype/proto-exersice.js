0d04a9fa636b33ef15335b7cf4bf482117992da6
shanwaz-78 <mohammed.shanwaz78@gmail.com>
2022-12-31 13:41:19 +0530

    learned new method & debug some issues in previous files as well

---
+++ Prototype/proto-exersice.js
@@ -0,0 +1,24 @@
+function method(a, b) {
+  this.a = a;
+  this.b = b;
+}
+
+method.prototype.divide = function () {
+  const obj = {
+    fn() {
+      return [this.a, this.b, "Hello World !"];
+    },
+  };
+  return obj;
+};
+
+const call = new divide(10, 20);
+console.log(call.divide().fn());
+
+const val = [1, 2, 3];
+Array.prototype.customMap = function () {
+  console.log(this);
+};
+
+val.customMap();
+[1, 2, 3].customMap();
