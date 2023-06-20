0890b8bdae8e4cd44dd95823ee1d54fc34ce646f
shanwaz-78 <111566952+shanwaz-78@users.noreply.github.com>
2023-06-20 18:26:12 +0530

    Delete anchor.js

--- string/anchor.js
+++
@@ -1,6 +0,0 @@
-// The anchor() method creates a string that embeds a string in an <a> element with a name (<a name="...">str</a>).
-
-const str = 'Table of Contents';
-document.body.innerHTML = str.anchor("just link");
-
-// Expected output <a name="just link">Table of Contents</a>
